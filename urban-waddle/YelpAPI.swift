da5487d872356d0c662d7a25806cc3fdf21b069d
Jacob <jacobsokora@gmail.com>
2018-04-17 13:09:53 -0500

    Stop printing yelp request urls

--- urban-waddle/YelpAPI.swift
+++ urban-waddle/YelpAPI.swift
@@ -104,7 +104,7 @@ struct YelpAPI {
         guard let url = urlComponents.url else {
             fatalError("Could not create url from components")
         }
-        print("\(url)")
+        //print("\(url)")
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
@@ -142,7 +142,7 @@ struct YelpAPI {
         guard let url = urlComponents.url else {
             fatalError("Could not create url from components")
         }
-        print("\(url)")
+        //print("\(url)")
         
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
